89de59e81143405d18c0c0e7b84ca56749163928
acmgit <undertakers_help@yahoo.com>
2022-02-08 19:50:03 +0100

    Join and leave messages in the irc added.

--- core.lua
+++ core.lua
@@ -1,4 +1,6 @@
 local sc = smart_chat
+local S = sc.S
+
 sc.crlf = "\r\n"
 
 minetest.register_on_chat_message(function(player, message)
@@ -17,24 +19,25 @@ minetest.register_on_chat_message(function(player, message)
 
 end) -- register_on_chatmessage()
 
-       
+
 minetest.register_on_joinplayer(function(player)
         local playername = player:get_player_name()
-        local line = S("joins the World")
+        local message = S("joins the World") .. "."
+        playername = playername or S("unknown")
+        sc.send_2_irc(playername, message)
         
         sc.player[playername] = nil -- the public Chat
         sc.public[playername] = nil
-        sc.send_2_irc(playername, "*** " .. line .. ".")
-        
+
 end) -- register_on_joinplayer()
 
 minetest.register_on_leaveplayer(function(player)
-        local playername = player:get_player_name()
-        local line = S("leaves the World")
-        
+        local playername = player:get_player_name() or S("unknown")
+        local line = S("leaves the World") .. "."
+
         sc.player[playername] = nil
         sc.public[playername] = nil
         sc.send_2_irc(playername, "*** " .. line .. ".")
-        
+
 end) -- minetest.register_on_leaveplayer
 
