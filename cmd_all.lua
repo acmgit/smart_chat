09c83149cf8521b037a74a28aa7c5105594f4599
acmgit <undertakers_help@yahoo.com>
2022-02-26 23:11:20 +0100

    ::::::::::::::

--- cmd_all.lua
+++ cmd_all.lua
@@ -16,7 +16,7 @@ sc.register_help({
                        )
 
 local all_send_to_irc = function (message)
-    local line = "PRIVMSG " .. sc.irc_channel .. " " .. message .. sc.crlf
+    local line = "PRIVMSG " .. sc.irc_channel .. ":" .. message .. sc.crlf
 
     sc.client:send(line)
     sc.irc_message_count = 1   -- This prevents for IRC-Echos of multiple player
@@ -62,7 +62,7 @@ sc.registered_commands[cname] = function(player, parameter)
 
         minetest.chat_send_all(line)
 
-        line = " [" .. player .. "@" .. sc.servername .. "] " .. message
+        line = "[" .. player .. "@" .. sc.servername .. "] " .. message
         if(sc.irc_on) then
             all_send_to_irc(line)
             minetest.log("action", line)
@@ -80,7 +80,7 @@ sc.registered_commands[cname] = function(player, parameter)
                                .. sc.yellow .. "] " .. sc.green .. message
         minetest.chat_send_all(line)
 
-        line = " [" .. player .. "@" .. sc.servername .. "] " .. message
+        line = "[" .. player .. "@" .. sc.servername .. "] " .. message
         if(sc.irc_on) then
             all_send_to_irc(line)
             minetest.log("action", line)
